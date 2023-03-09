14d20707cf39433bf0ed3396dbe5dc2563563983
thaolaptrinh <thaolaptrinh@gmail.com>
2023-03-09 19:03:22 +0700

    update

---
+++ bootstrap.php
@@ -0,0 +1,13 @@
+<?php
+
+use App\Core\Database;
+use App\Core\Model;
+
+date_default_timezone_set('Asia/Ho_Chi_Minh');
+ini_set('display_errors', 'on');
+
+
+require_once 'mainConfig.php';
+require_once 'vendor/autoload.php';
+require_once 'libs/function.php';
+require_once 'app/App.php';
---
+++ index.php
@@ -0,0 +1,5 @@
+<?php
+session_start();
+require_once 'bootstrap.php';
+
+$myApp = new App();
